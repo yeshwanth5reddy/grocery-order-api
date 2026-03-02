e9cb357a93bfacfe76f07aeab8438b37694a438c
Yeshwanth Reddy <saiyeshwanthred.alla@servicenow.com>
2026-03-02 21:18:27 +0530

    Day1: project setup with springboot3,java 21, postgre sql, mongo db

---
+++ src/main/java/com/groceryorder/GroceryOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.groceryorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryOrderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groceryorder/GroceryOrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groceryorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryOrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
